90f91817988e443977a0e5d9236e3ce58d579ca1
rridley <rridley@port53.me>
2023-09-28 17:08:03 -0400

    Add small note

--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -1,10 +1,28 @@
 import { Controller, Get, Route } from "tsoa";
 import { Group } from "../models/examples";
 
+// Uncommenting the import and other controller method will let this build successfully again
+
+//import { ExampleBranch } from "../models/generated";
+
 @Route("example")
 export class ExampleController extends Controller {
+  /*
+  @Get("/test2")
+  public async getExample2(): Promise<Array<ExampleBranch>> {
+
+    const data: ExampleBranch = {
+        id: "1",
+        name: "test",
+    };
+
+    return [data];
+  }
+  */
+
   @Get("/")
   public async getExample(): Promise<Group> {
+
     const data: Group = {
       id: "1",
       site: {
